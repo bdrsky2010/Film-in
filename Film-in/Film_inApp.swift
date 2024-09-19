30dcf93921ff06cfae5ae1d8b5f4ee73fe70ad59
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-19 18:39:10 +0900

    Initial commit

---
+++ Film-in/Film_inApp.swift
@@ -0,0 +1,17 @@
+//
+//  Film_inApp.swift
+//  Film-in
+//
+//  Created by Minjae Kim on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Film_inApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Film-in
+ Film-in
