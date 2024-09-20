986d8eb0b47f05362429748e743d664d78f7d895
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-20 23:50:42 +0900

    💾 Add: TMDB API 호출 시 사용될 헤더를 담고있는 enum 추가

---
+++ Film-in/Common/Network/TMDBHeader.swift
@@ -0,0 +1,14 @@
+//
+//  TMDBHeader.swift
+//  Film-in
+//
+//  Created by Minjae Kim on 9/20/24.
+//
+
+import Foundation
+
+enum TMDBHeader: String {
+    case authorization = "Authorization"
+    case accept = "accept"
+    case json = "application/json"
+}
