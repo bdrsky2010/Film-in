7a9ecdf8c37f4f73eace831701c7c516fbd13283
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-20 23:47:43 +0900

    💾 Add: 다국어 처리된 문자열 반환받을 연산 프로퍼티를 String extension에 추가

---
+++ Film-in/Resources/Extension/String+.swift
@@ -0,0 +1,14 @@
+//
+//  String+.swift
+//  Film-in
+//
+//  Created by Minjae Kim on 9/20/24.
+//
+
+import Foundation
+
+extension String {
+    var localized: Self {
+        return NSLocalizedString(self, comment: "")
+    }
+}
