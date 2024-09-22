93423ab2b87ae452b7d3edd2273abd267f0f0bcf
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-22 23:51:09 +0900

    💾 Add: 다른 DTO에서 공통적으로 사용되는 기간 타입

---
+++ Film-in/Common/Data/PeriodResponseDTO.swift
@@ -0,0 +1,13 @@
+//
+//  PeriodResponseDTO.swift
+//  Film-in
+//
+//  Created by Minjae Kim on 9/22/24.
+//
+
+import Foundation
+
+struct PeriodResponseDTO: Decodable {
+    let maximum: String
+    let minimum: String
+}
