7f61c4f90fe445032e64e6f79adaddb7ba3b7c74
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-23 23:58:06 +0900

    💾 Add: TMDB API Router에 대한 사용 API 추가로 Case 계속해서 추가중

--- Film-in/Common/Network/TMDBRouter.swift
+++ Film-in/Common/Network/TMDBRouter.swift
@@ -19,6 +19,14 @@ enum TMDBRouter {
     case trending(_ dto: TrendingRequestDTO)
     case nowPlaying(_ dto: NowPlayingRequestDTO)
     case upcoming(_ dto: UpcomingRequestDTO)
+    case discover(_ dto: DiscoverRequestDTO)
+    case searchMovie(_ dto: SearchMovieRequestDTO)
+    case searchPerson(_ dto: SearchPersonRequestDTO)
+    case movieDetail(_ dto: MovieDetailRequestDTO, movieId: String)
+    case movieCredit(_ dto: MovieCreditRequestDTO, movieId: String)
+    case movieSimiliar(_ dto: MovieSimiliarRequestDTO, movieId: String)
+    case movieRecommendation(_ dto: MovieRecommendationRequestDTO, movieId: String)
+    case movieImage(_ dto: MovieImageRequestDTO, movieId: String)
 }
 
 extension TMDBRouter: TMDBTargetType {
@@ -28,17 +36,18 @@ extension TMDBRouter: TMDBTargetType {
         case .trending: return "trending/movie/day"
         case .nowPlaying: return "movie/now_playing"
         case .upcoming: return "movie/upcoming"
+        case .discover: return "discover/movie"
+        case .searchMovie: return "search/movie"
+        case .searchPerson: return "search/person"
+        case .movieDetail(_, let movieId): return "movie/\(movieId)"
+        case .movieCredit(_, let movieId): return "movie/\(movieId)/credits"
+        case .movieSimiliar(_, let movieId): return "movie/\(movieId)/similar"
+        case .movieRecommendation(_, let movieId): return "movie/\(movieId)/recommendations"
+        case .movieImage(_, let movieId): return "movie/\(movieId)/images"
         }
     }
     
-    var method: Moya.Method {
-        switch self {
-        case .genres: return .get
-        case .trending: return .get
-        case .nowPlaying: return .get
-        case .upcoming: return .get
-        }
-    }
+    var method: Moya.Method { .get }
     
     var task: Moya.Task {
         switch self {
@@ -50,31 +59,29 @@ extension TMDBRouter: TMDBTargetType {
             return .requestParameters(parameters: dto.asParameters, encoding: URLEncoding.queryString)
         case .upcoming(let dto):
             return .requestParameters(parameters: dto.asParameters, encoding: URLEncoding.queryString)
+        case .discover(let dto):
+            return .requestParameters(parameters: dto.asParameters, encoding: URLEncoding.queryString)
+        case .searchMovie(let dto):
+            return .requestParameters(parameters: dto.asParameters, encoding: URLEncoding.queryString)
+        case .searchPerson(let dto):
+            return .requestParameters(parameters: dto.asParameters, encoding: URLEncoding.queryString)
+        case .movieDetail(let dto, _):
+            return .requestParameters(parameters: dto.asParameters, encoding: URLEncoding.queryString)
+        case .movieCredit(let dto, _):
+            return .requestParameters(parameters: dto.asParameters, encoding: URLEncoding.queryString)
+        case .movieSimiliar(let dto, _):
+            return .requestParameters(parameters: dto.asParameters, encoding: URLEncoding.queryString)
+        case .movieRecommendation(let dto, _):
+            return .requestParameters(parameters: dto.asParameters, encoding: URLEncoding.queryString)
+        case .movieImage(let dto, _):
+            return .requestParameters(parameters: dto.asParameters, encoding: URLEncoding.queryString)
         }
     }
     
     var headers: [String : String]? {
-        switch self {
-        case .genres:
-            return [
-                TMDBHeader.authorization.rawValue: APIKEY.tmdb,
-                TMDBHeader.accept.rawValue: TMDBHeader.json.rawValue
-            ]
-        case .trending:
-            return [
-                TMDBHeader.authorization.rawValue: APIKEY.tmdb,
-                TMDBHeader.accept.rawValue: TMDBHeader.json.rawValue
-            ]
-        case .nowPlaying:
-            return [
-                TMDBHeader.authorization.rawValue: APIKEY.tmdb,
-                TMDBHeader.accept.rawValue: TMDBHeader.json.rawValue
-            ]
-        case .upcoming:
-            return [
-                TMDBHeader.authorization.rawValue: APIKEY.tmdb,
-                TMDBHeader.accept.rawValue: TMDBHeader.json.rawValue
-            ]
-        }
+        return [
+            TMDBHeader.authorization.rawValue: APIKEY.tmdb,
+            TMDBHeader.accept.rawValue: TMDBHeader.json.rawValue
+        ]
     }
 }
