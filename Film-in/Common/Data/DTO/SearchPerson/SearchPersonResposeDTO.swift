d981f3ca065029bf90505a0826ca0261c273a7c3
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-24 22:48:56 +0900

    🩹 Refactor: 재사용될 줄 알았던 타입을 외부에서 내부로 다시 가져옴

--- Film-in/Common/Data/DTO/SearchPerson/SearchPersonResposeDTO.swift
+++ Film-in/Common/Data/DTO/SearchPerson/SearchPersonResposeDTO.swift
@@ -8,8 +8,32 @@
 import Foundation
 
 struct SearchPersonResposeDTO: Decodable {
+    struct Person: Decodable {
+        let adult: Bool
+        let gender: Int
+        let id: Int
+        let department: String
+        let name: String
+        let originName: String
+        let popularity: Double
+        let profilePath: String
+        let knownFor: [TMDBMovieResponseDTO]
+        
+        enum CodingKeys: String, CodingKey {
+            case adult
+            case gender
+            case id
+            case department = "known_for_department"
+            case name
+            case originName = "original_name"
+            case popularity
+            case profilePath = "profile_path"
+            case knownFor = "known_for"
+        }
+    }
+    
     let page: Int
-    let results: [TMDBPersonResponseDTO]
+    let results: [Person]
     let totalPage: Int
     
     enum CodingKeys: String, CodingKey {
