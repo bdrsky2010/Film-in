c9030273f5835edcd142232044b2a446872ef09b
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-26 23:37:27 +0900

    💾 Add: TMDB API 요청 메서드 추가 구성
    - Trending
    - NowPlaying
    - Upcoming
    - Discover

--- Film-in/Common/Network/TMDBRepository.swift
+++ Film-in/Common/Network/TMDBRepository.swift
@@ -9,6 +9,10 @@ import Foundation
 
 protocol TMDBRepository: AnyObject {
     func movieGenreRequest(query: MovieGenreQuery) async -> Result<[MovieGenre], TMDBError>
+    func trendingRequest(query: TrendingQuery) async -> Result<HomeMovie, TMDBError>
+    func nowPlayingRequest(query: HomeMovieQuery) async -> Result<HomeMovie, TMDBError>
+    func upcomingRequest(query: HomeMovieQuery) async -> Result<HomeMovie, TMDBError>
+    func discoverRequest(query: HomeMovieQuery) async -> Result<HomeMovie, TMDBError>
 }
 
 final class DefaultTMDBRepository: TMDBRepository {
@@ -23,6 +27,72 @@ final class DefaultTMDBRepository: TMDBRepository {
     func movieGenreRequest(query: MovieGenreQuery) async -> Result<[MovieGenre], TMDBError> {
         let requestDTO = GenreRequestDTO(language: query.language)
         let result = await networkManager.request(.genres(requestDTO), of: GenreResponseDTO.self)
+        switch result {
+        case .success(let success):
+            return .success(success.toEntity)
+        case .failure(let failure):
+            return .failure(failure)
+        }
+    }
+    
+    func trendingRequest(query: TrendingQuery) async -> Result<HomeMovie, TMDBError> {
+        let requestDTO = TrendingRequestDTO(language: query.language)
+        let result = await networkManager.request(.trending(requestDTO), of: TrendingResponseDTO.self)
+        switch result {
+        case .success(let success):
+            return .success(success.toEntity)
+        case .failure(let failure):
+            return .failure(failure)
+        }
+    }
+    
+    func nowPlayingRequest(query: HomeMovieQuery) async -> Result<HomeMovie, TMDBError> {
+        let requestDTO = NowPlayingRequestDTO(
+            language: query.language,
+            page: query.page,
+            region: query.region
+        )
+        let result = await networkManager.request(
+            .nowPlaying(requestDTO),
+            of: NowPlayingResponseDTO.self
+        )
+        switch result {
+        case .success(let success):
+            return .success(success.toEntity)
+        case .failure(let failure):
+            return .failure(failure)
+        }
+    }
+    
+    func upcomingRequest(query: HomeMovieQuery) async -> Result<HomeMovie, TMDBError> {
+        let requestDTO = UpcomingRequestDTO(
+            language: query.language,
+            page: query.page,
+            region: query.region
+        )
+        let result = await networkManager.request(
+            .upcoming(requestDTO),
+            of: UpcomingResponseDTO.self
+        )
+        switch result {
+        case .success(let success):
+            return .success(success.toEntity)
+        case .failure(let failure):
+            return .failure(failure)
+        }
+    }
+    
+    func discoverRequest(query: HomeMovieQuery) async -> Result<HomeMovie, TMDBError> {
+        let requestDTO = DiscoverRequestDTO(
+            language: query.language,
+            page: query.page,
+            region: query.region,
+            withGenres: query.withGenres ?? ""
+        )
+        let result = await networkManager.request(
+            .discover(requestDTO),
+            of: DiscoverResponseDTO.self
+        )
         switch result {
         case .success(let success):
             return .success(success.toEntity)
