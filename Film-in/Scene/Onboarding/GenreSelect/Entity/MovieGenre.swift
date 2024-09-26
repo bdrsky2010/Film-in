67a766289e19552479c2bcb989eab0d62379cef8
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-26 23:35:54 +0900

    💾 Add: 장르 선택화면에서 사용되는 장르에 대한 타입 구성

---
+++ Film-in/Scene/Onboarding/GenreSelect/Entity/MovieGenre.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGenre.swift
+//  Film-in
+//
+//  Created by Minjae Kim on 9/24/24.
+//
+
+import Foundation
+
+struct MovieGenre: Hashable, Identifiable {
+    let id: Int
+    let name: String
+}
