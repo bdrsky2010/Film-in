633a36380fdb0aa05b2399b2bb51ae584fbdfb2c
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-27 23:40:43 +0900

    💾 Add: 최상단 탭바 구성중

---
+++ Film-in/Scene/MainTabView/MainTabView.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabView.swift
+//  Film-in
+//
+//  Created by Minjae Kim on 9/25/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            HomeView(
+                viewModel: HomeViewModel(
+                    homeService: DefaultHomeService(
+                        tmdbRepository: DefaultTMDBRepository.shared,
+                        databaseRepository: RealmRepository.shared
+                    ),
+                    networkMonitor: NetworkMonitor.shared
+                )
+            )
+                .tabItem {
+                    Image("home")
+                    Text("HOME")
+                }
+            
+            Text("done")
+                .tabItem {
+                    Image("search")
+                    Text("SEARCH")
+                }
+            
+            Text("done")
+                .tabItem {
+                    Image("movie")
+                    Text("MY")
+                }
+        }
+        .tint(.app)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
