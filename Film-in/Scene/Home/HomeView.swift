9fdd02192ecf12dd2ee20ddd3608c0d132332938
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-30 03:45:44 +0900

    🩹 Refactor: 필요없는 코드 삭제 및 네비게이션 스택 embed

--- Film-in/Scene/Home/HomeView.swift
+++ Film-in/Scene/Home/HomeView.swift
@@ -8,19 +8,13 @@
 import SwiftUI
 import PopupView
 
-struct Card: Hashable, Identifiable {
-    let id = UUID()
-    var title: String
-}
-
 struct HomeView: View {
     @StateObject private var viewModel: HomeViewModel
     @Namespace var namespace
     @State private var index = 0
-    @State private var items = [Card(title: "1"), Card(title: "2"), Card(title: "3"), Card(title: "4")]
     @State private var showDetailView = false
     @State private var movie: HomeMovie.Movie?
-    @State private var cardSize: CGSize = .zero
+    @State private var posterSize: CGSize = .zero
     @State private var offset: CGFloat = 0
     
     init(viewModel: HomeViewModel) {
@@ -29,154 +23,149 @@ struct HomeView: View {
     
     // poster size 1 x 1.5
     var body: some View {
-        if !viewModel.output.networkConnect {
-            NotConnectView(viewModel: viewModel)
-        } else {
-            GeometryReader { proxy in
-                VStack {
-                    Text("Film-in")
-                        .font(.ibmPlexMonoSemiBold(size: 50))
-                        .foregroundStyle(.app)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal)
-                        .frame(height: 50)
-                    
-                    ScrollView {
-                        SnapCarousel(spacing: 28, trailingSpace: 120, index: $index, items: viewModel.output.trendingMovies.movies) { movie in
+        NavigationStack {
+            if !viewModel.output.networkConnect {
+                NotConnectView(viewModel: viewModel)
+            } else {
+                GeometryReader { proxy in
+                    VStack {
+                        Text("Film-in")
+                            .font(.ibmPlexMonoSemiBold(size: 50))
+                            .foregroundStyle(.app)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal)
+                            .frame(height: 50)
+                        
+                        ScrollView {
+                            SnapCarousel(spacing: 28, trailingSpace: 120, index: $index, items: viewModel.output.trendingMovies.movies) { movie in
+                                
+                                let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
+                                PosterImage(url: url, size: posterSize, title: movie.title)
+                                    .matchedGeometryEffect(id: movie.id, in: namespace)
+                                    .onTapGesture {
+                                        self.movie = movie
+                                        withAnimation(.easeInOut) {
+                                            showDetailView = true
+                                        }
+                                    }
+                            }
+                            .frame(height: posterSize.height)
+                            .padding(.bottom, 20)
                             
-                            let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
-                            PosterImage(url: url, size: cardSize, title: movie.title)
-                                .matchedGeometryEffect(id: movie.id, in: namespace)
-                                .onTapGesture {
-                                    self.movie = movie
-                                    withAnimation(.easeInOut) {
-                                        showDetailView = true
+                            ListHeader(title: "nowPlaying", usedTo: .nowPlaying)
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                LazyHStack {
+                                    ForEach(viewModel.output.nowPlayingMovies.movies, id: \.id) { movie in
+                                        let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
+                                        PosterImage(url: url, size: CGSize(width: posterSize.width * 0.5, height: posterSize.height * 0.5), title: movie.title)
+                                            .padding(.bottom, 4)
+                                            .padding(.horizontal, 8)
+                                            .matchedGeometryEffect(id: movie.id, in: namespace)
+                                            .onTapGesture {
+                                                self.movie = movie
+                                                withAnimation(.easeInOut) {
+                                                    showDetailView = true
+                                                }
+                                            }
                                     }
                                 }
-                        }
-                        .frame(height: cardSize.height)
-                        .padding(.bottom, 20)
-                        
-                        ListHeader(title: "nowPlaying")
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            LazyHStack {
-                                ForEach(viewModel.output.nowPlayingMovies.movies, id: \.id) { movie in
-                                    let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
-                                    PosterImage(url: url, size: CGSize(width: cardSize.width * 0.5, height: cardSize.height * 0.5), title: movie.title)
-                                        .padding(.bottom, 4)
-                                        .padding(.horizontal, 8)
-                                        .matchedGeometryEffect(id: movie.id, in: namespace)
-                                        .onTapGesture {
-                                            self.movie = movie
-                                            withAnimation(.easeInOut) {
-                                                showDetailView = true
+                            }
+                            
+                            ListHeader(title: "upcoming", usedTo: .upcoming)
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                LazyHStack {
+                                    ForEach(viewModel.output.upcomingMovies.movies, id: \.id) { movie in
+                                        let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
+                                        PosterImage(url: url, size: CGSize(width: posterSize.width * 0.5, height: posterSize.height * 0.5), title: movie.title)
+                                            .padding(.bottom, 4)
+                                            .padding(.horizontal, 8)
+                                            .matchedGeometryEffect(id: movie.id, in: namespace)
+                                            .onTapGesture {
+                                                self.movie = movie
+                                                withAnimation(.easeInOut) {
+                                                    showDetailView = true
+                                                }
                                             }
-                                        }
+                                    }
+                                }
+                            }
+                            
+                            ListHeader(title: "recommend", usedTo: .recommend)
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                LazyHStack {
+                                    ForEach(viewModel.output.recommendMovies.movies, id: \.id) { movie in
+                                        let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
+                                        PosterImage(url: url, size: CGSize(width: posterSize.width * 0.5, height: posterSize.height * 0.5), title: movie.title)
+                                            .padding(.bottom, 4)
+                                            .padding(.horizontal, 8)
+                                            .matchedGeometryEffect(id: movie.id, in: namespace)
+                                            .onTapGesture {
+                                                self.movie = movie
+                                                withAnimation(.easeInOut) {
+                                                    showDetailView = true
+                                                }
+                                            }
+                                    }
                                 }
                             }
                         }
-                        
-                        
-                        ListHeader(title: "upcoming")
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            LazyHStack {
-                                ForEach(viewModel.output.upcomingMovies.movies, id: \.id) { movie in
-                                    let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
-                                    PosterImage(url: url, size: CGSize(width: cardSize.width * 0.5, height: cardSize.height * 0.5), title: movie.title)
-                                        .padding(.bottom, 4)
-                                        .padding(.horizontal, 8)
-                                        .matchedGeometryEffect(id: movie.id, in: namespace)
-                                        .onTapGesture {
-                                            self.movie = movie
-                                            withAnimation(.easeInOut) {
-                                                showDetailView = true
-                                            }
-                                        }
-                                }
-                            }
-                        }
-                        
-                        
-                        ListHeader(title: "recommend")
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            LazyHStack {
-                                ForEach(viewModel.output.recommendMovies.movies, id: \.id) { movie in
-                                    let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
-                                    PosterImage(url: url, size: CGSize(width: cardSize.width * 0.5, height: cardSize.height * 0.5), title: movie.title)
-                                        .padding(.bottom, 4)
-                                        .padding(.horizontal, 8)
-                                        .matchedGeometryEffect(id: movie.id, in: namespace)
-                                        .onTapGesture {
-                                            self.movie = movie
-                                            withAnimation(.easeInOut) {
-                                                showDetailView = true
-                                            }
-                                        }
-                                }
-                            }
-                        }
+                    }
+                    .task {
+                        posterSize = CGSize(width: proxy.size.width * 0.7, height: proxy.size.width * 0.7 * 1.5)
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                .overlay {
+                    if let movie, showDetailView {
+                        MovieDetailView(
+                            offset: $offset,
+                            showDetailView: $showDetailView,
+                            namespace: namespace,
+                            movie: movie,
+                            size: posterSize
+                        )
                     }
                 }
                 .task {
-                    cardSize = CGSize(width: proxy.size.width * 0.7, height: proxy.size.width * 0.7 * 1.5)
-                }
-                
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-            .overlay {
-                if let movie, showDetailView {
-                    MovieDetailView(
-                        offset: $offset,
-                        showDetailView: $showDetailView,
-                        namespace: namespace,
-                        movie: movie,
-                        size: cardSize
-                    )
-                }
-            }
-            .task {
-                withAnimation {
-                    viewModel.action(.viewOnTask)
-                }
-            }
-            .popup(isPresented: $viewModel.output.isShowAlert) {
-                VStack {
-                    Text("apiRequestError")
-                        .font(.ibmPlexMonoSemiBold(size: 20))
-                    Button {
-                        viewModel.action(.refresh)
-                    } label: {
-                        Text("refresh")
-                            .font(.ibmPlexMonoMedium(size: 20))
-                            .underline()
-                            .foregroundStyle(.white)
+                    withAnimation {
+                        viewModel.action(.viewOnTask)
                     }
-                    .padding(.bottom, 4)
                 }
-                .frame(maxWidth: .infinity)
-                .background(.red)
-            } customize: {
-                $0
-                    .type(.floater(verticalPadding: 0, horizontalPadding: 0, useSafeAreaInset: true))
-                    .animation(.bouncy)
-                    .position(.top)
-                    .dragToDismiss(true)
+                .popup(isPresented: $viewModel.output.isShowAlert) {
+                    VStack {
+                        Text("apiRequestError")
+                            .font(.ibmPlexMonoSemiBold(size: 20))
+                        Button {
+                            viewModel.action(.refresh)
+                        } label: {
+                            Text("refresh")
+                                .font(.ibmPlexMonoMedium(size: 20))
+                                .underline()
+                                .foregroundStyle(.white)
+                        }
+                        .padding(.bottom, 4)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(.red)
+                } customize: {
+                    $0
+                        .type(.floater(verticalPadding: 0, horizontalPadding: 0, useSafeAreaInset: true))
+                        .animation(.bouncy)
+                        .position(.top)
+                        .dragToDismiss(true)
+                }
             }
         }
     }
 }
 
-struct ListHeader: View {
-    enum ListType {
-        case nowPlaying
-        case upcoming
-        case recommend
-    }
-    
+fileprivate struct ListHeader: View {
     let title: LocalizedStringKey
+    let usedTo: UsedTo
     
     var body: some View {
         HStack(alignment: .lastTextBaseline) {
@@ -187,10 +176,8 @@ struct ListHeader: View {
             
             Spacer()
             
-            Button {
-                withAnimation {
-                    
-                }
+            NavigationLink {
+                SeeMoreView(usedTo: usedTo)
             } label: {
                 Text("more")
                     .font(.ibmPlexMonoSemiBold(size: 16))
