d1dfde94e98de45d12ae2837f55b1a7539d12781
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-10-01 23:26:37 +0900

    💾 Add: Realm db CRUD 메서드 구성
     삭제 및 수정 등 추후 추가 예정

--- Film-in/Common/Database/RealmRepository.swift
+++ Film-in/Common/Database/RealmRepository.swift
@@ -11,6 +11,11 @@ import RealmSwift
 protocol DatabaseRepository: AnyObject {
     func createUser()
     func appendLikeGenres(genres: Set<MovieGenre>)
+    func appendWantMovie(data: WantWatchedMovieRequestDTO)
+    func appendWatchedMovie(data: WantWatchedMovieRequestDTO)
+    func deleteWatchedMovie(movieId: Int)
+    func deleteWantMovie(movieId: Int)
+    func printFilePath()
     
     var user: UserTable? { get }
 }
@@ -42,5 +47,81 @@ final class RealmRepository: DatabaseRepository {
         } catch {
             // TODO: Update Genres Error Handling
         }
+    }
+    
+    func appendWantMovie(data: WantWatchedMovieRequestDTO) {
+        guard let user else { return }
+        do {
+            try realm.write {
+                if let movie = user.watchedMovies.last(where: { $0.id == data.movieId }) {
+                    realm.delete(movie)
+                } else if let movie = user.wantMovies.last(where: { $0.id == data.movieId }) {
+                    realm.delete(movie)
+                }
+                let newMovie = MovieTable(
+                    id: data.movieId,
+                    title: data.title,
+                    backdrop: data.backdrop,
+                    poster: data.poster,
+                    status: data.type == .want ? (data.isAlarm ? .wantWithNotification : .want) : .watched,
+                    date: data.date
+                )
+                user.wantMovies.append(newMovie)
+            }
+        } catch {
+            
+        }
+    }
+    
+    func appendWatchedMovie(data: WantWatchedMovieRequestDTO) {
+        guard let user else { return }
+        do {
+            try realm.write {
+                if let movie = user.wantMovies.last(where: { $0.id == data.movieId }) {
+                    realm.delete(movie)
+                }
+                let newMovie = MovieTable(
+                    id: data.movieId,
+                    title: data.title,
+                    backdrop: data.backdrop,
+                    poster: data.poster,
+                    status: data.type == .want ? (data.isAlarm ? .wantWithNotification : .want) : .watched,
+                    date: data.date
+                )
+                user.watchedMovies.append(newMovie)
+            }
+        } catch {
+            
+        }
+    }
+    
+    func deleteWantMovie(movieId: Int) {
+        guard let user else { return }
+        do {
+            try realm.write {
+                if let movie = user.wantMovies.last(where: { $0.id == movieId }) {
+                    realm.delete(movie)
+                }
+            }
+        } catch {
+            
+        }
+    }
+    
+    func deleteWatchedMovie(movieId: Int) {
+        guard let user else { return }
+        do {
+            try realm.write {
+                if let movie = user.watchedMovies.last(where: { $0.id == movieId }) {
+                    realm.delete(movie)
+                }
+            }
+        } catch {
+            
+        }
+    }
+    
+    func printFilePath() {
+        print(Realm.Configuration.defaultConfiguration.fileURL ?? "")
     }
 }
