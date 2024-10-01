abd8f77878a36efc6ecd64046ec92e865d3bffc5
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-10-01 23:28:37 +0900

    💾 Add: 앱이 켜져있는 상태에서도 푸시 알림을 받도록 하기 위해 델리게이트 채택

--- Film-in/Film_inApp.swift
+++ Film-in/Film_inApp.swift
@@ -8,23 +8,40 @@
 import SwiftUI
 import FirebaseCore
 
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        return true
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge])
+    }
 }
 
 @main
 struct Film_inApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @AppStorage("onboarding") private var isOnboarding = false
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if isOnboarding {
+                MainTabView()
+            } else {
+                GenreSelectView(
+                    viewModel: GenreSelectViewModel(
+                        genreSelectService: DefaultGenreSelectService(tmdbRepository: DefaultTMDBRepository.shared, databaseRepository: RealmRepository.shared),
+                        networkMonitor: NetworkMonitor.shared
+                    )
+                )
+            }
         }
     }
 }
