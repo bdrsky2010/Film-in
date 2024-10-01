b92ceeb1108b21fc0e3a5f84f34a8925ec4542ae
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-10-01 23:25:17 +0900

    💾 Add: Realm Movie 테이블 구성

--- Film-in/Common/Database/RealmModel/UserTable.swift
+++ Film-in/Common/Database/RealmModel/UserTable.swift
@@ -8,12 +8,45 @@
 import Foundation
 import RealmSwift
 
+enum MovieWatchStatus: Int, PersistableEnum {
+    case wantWithNotification
+    case want
+    case watched
+}
+
 final class UserTable: Object, ObjectKeyIdentifiable {
     @Persisted(primaryKey: true) var id: ObjectId
     @Persisted var selectedGenreIds: List<Int>
+    @Persisted var wantMovies: List<MovieTable>
+    @Persisted var watchedMovies: List<MovieTable>
     
-    convenience init(selectedGenreIds: List<Int>) {
+    convenience init(
+        selectedGenreIds: List<Int>,
+        wantMovies: List<MovieTable> = List<MovieTable>(),
+        watchedMovies: List<MovieTable> = List<MovieTable>()
+    ) {
         self.init()
         self.selectedGenreIds = selectedGenreIds
+        self.wantMovies = wantMovies
+        self.watchedMovies = watchedMovies
+    }
+}
+
+final class MovieTable: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var id: Int
+    @Persisted var title: String
+    @Persisted var backdrop: String
+    @Persisted var poster: String
+    @Persisted var status: MovieWatchStatus
+    @Persisted var date: Date
+    
+    convenience init(id: Int, title: String, backdrop: String, poster: String, status: MovieWatchStatus, date: Date) {
+        self.init()
+        self.id = id
+        self.title = title
+        self.backdrop = backdrop
+        self.poster = poster
+        self.status = status
+        self.date = date
     }
 }
