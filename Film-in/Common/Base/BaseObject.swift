5839947cdeb0bb3a58022cb2147b29e415e80606
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-10-07 23:07:06 +0900

    🩹 Refactor: 모든 class에 적용하기 위해 이름을 변경

--- Film-in/Common/Base/BaseObject.swift
+++ Film-in/Common/Base/BaseObject.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-class BaseViewModel: ObservableObject {
+class BaseObject: ObservableObject {
     init() {
         print("\(String(describing: self)) is init")
     }
