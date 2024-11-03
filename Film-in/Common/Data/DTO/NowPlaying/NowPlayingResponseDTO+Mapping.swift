33ed5bcef40e23adb44a84064cd31dde9cd10720
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-11-03 23:58:09 +0900

    👾 Fix: 현재상영작 더보기 화면에서 페이지네이션 안되는 버그 - 테스트 완료
    API 데이터 파싱했을 때 totalPage 요소를 받지를 않아 뷰모델에서 if-let 구문을 통한 옵셔널 바인딩이
    되지 않는 현상으로 인해 현재 받아온 페이지 수의 얻데이트가 이뤄지지 않아 계속 같은 1페이지를
    받아오고있었음

--- Film-in/Common/Data/DTO/NowPlaying/NowPlayingResponseDTO+Mapping.swift
+++ Film-in/Common/Data/DTO/NowPlaying/NowPlayingResponseDTO+Mapping.swift
@@ -11,6 +11,14 @@ struct NowPlayingResponseDTO: Decodable {
     let dates: PeriodResponseDTO
     let page: Int
     let results: [TMDBMovieResponseDTO]
+    let totalPages: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case dates
+        case page
+        case results
+        case totalPages = "total_pages"
+    }
 }
 
 extension NowPlayingResponseDTO {
@@ -21,6 +29,7 @@ extension NowPlayingResponseDTO {
                 maximum: dates.maximum
             ),
             page: self.page,
+            totalPage: self.totalPages,
             movies: self.results
                 .map {
                     HomeMovie.Movie(
