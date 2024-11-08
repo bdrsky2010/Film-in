09d94a79f44cd9b1f87910eb8da40fe046984aaf
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-11-08 23:02:13 +0900

    💾 Add: 날짜 생성 메서드 및 해당 날짜에 데이터가 있는 지 빠른 탐색을 위한 이진탐색 메서드 추가

--- Film-in/Scene/MyView/Service/MyViewService.swift
+++ Film-in/Scene/MyView/Service/MyViewService.swift
@@ -6,23 +6,30 @@
 //
 
 import Foundation
+import RealmSwift
 
 protocol MyViewService: AnyObject {
     func requestDeleteMovie(movieId: Int)
+    func generateDays() -> [Day]
 }
 
-final class DefaultMyViewService: BaseObject, MyViewService {
+final class DefaultMyViewService: BaseObject {
     private let databaseRepository: DatabaseRepository
     private let localNotificationManager: LocalNotificationManager
+    private let calendarManager: CalendarManager
     
     init(
         databaseRepository: DatabaseRepository,
-        localNotificationManager: LocalNotificationManager
+        localNotificationManager: LocalNotificationManager,
+        calendarManager: CalendarManager
     ) {
         self.databaseRepository = databaseRepository
         self.localNotificationManager = localNotificationManager
+        self.calendarManager = calendarManager
     }
-    
+}
+
+extension DefaultMyViewService: MyViewService {
     func requestDeleteMovie(movieId: Int) {
         databaseRepository.deleteMovie(movieId: movieId)
         
@@ -37,5 +44,34 @@ final class DefaultMyViewService: BaseObject, MyViewService {
         } catch {
             print(error)
         }
+    }
+    
+    func generateDays() -> [Day] {
+        guard let user = databaseRepository.user else { return [] }
+        
+        let days = calendarManager.generateDays()
+        
+        let result = days.map {
+            let isData = binarySearchMovie(user.wantMovies, $0) || binarySearchMovie(user.watchedMovies, $0)
+            return Day(date: $0, isData: isData)
+        }
+        
+        return result
+    }
+    
+    private func binarySearchMovie(_ list: List<MovieTable>, _ target: Date) -> Bool {
+        var left = 0
+        var right = list.count - 1
+        var mid: Int
+        
+        while left <= right {
+            mid = (left + right) / 2
+
+            if list[mid].date == target { return true }
+            else if list[mid].date > target { right = mid - 1 }
+            else { left = mid + 1 }
+        }
+        
+        return false
     }
 }
