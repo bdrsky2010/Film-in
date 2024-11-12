f744834b575350d117a9666d690a65d20d021e97
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-11-12 17:34:27 +0900

    Add: 커스텀 달력 추가

---
+++ Film-in/Scene/MyView/CalendarView.swift
@@ -0,0 +1,92 @@
+//
+//  CalendarView.swift
+//  Film-in
+//
+//  Created by Minjae Kim on 11/5/24.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    @ObservedObject var viewModel: MyViewModel
+    private let columns = Array(repeating: GridItem(.flexible()), count: 7)
+    private let weekDays = Calendar.current.shortWeekdaySymbols
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(weekDays, id: \.self) { day in
+                    Text(day)
+                        .font(.headline)
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 8)
+                }
+            }
+            
+            LazyVGrid(columns: columns, spacing: 15) {
+                ForEach(viewModel.output.selectMonthDays, id: \.id) { value in
+                    DayView(
+                        select: viewModel.output.selectDate,
+                        currentMonth: viewModel.output.currentMonth,
+                        value: value
+                    )
+                    .onTapGesture {
+                        viewModel.action(.selectDay(day: value.date))
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct DayView: View {
+    let select: Date
+    let currentMonth: Date
+    let value: Day
+        
+    var body: some View {
+        let calendar = Calendar.current
+        let current = calendar.dateComponents([.year, .month], from: currentMonth)
+        let date = calendar.dateComponents([.year, .month, .day], from: value.date)
+        let selectDate = calendar.dateComponents([.year, .month, .day], from: select)
+        let isSelect = date.year == selectDate.year && date.month == selectDate.month && date.day == selectDate.day
+        
+        if current.year == date.year && current.month == date.month {
+            ZStack(alignment: .bottom) {
+                if isSelect {
+                    GeometryReader { proxy in
+                        Rectangle()
+                            .frame(height: proxy.size.width)
+                            .foregroundStyle(.app)
+                    }
+                }
+                
+                Text("\(date.day ?? 0)")
+                    .font(.headline)
+                    .padding(.vertical, 8)
+                if value.isData {
+                    Circle()
+                        .frame(width: 5, height: 5)
+                }
+            }
+            .foregroundStyle(isSelect ? .appLightGray : .appText)
+            
+        } else {
+            Text("")
+                .font(.headline)
+                .padding(.vertical, 8)
+        }
+    }
+}
+
+#Preview {
+    MyView(
+        viewModel: MyViewModel(
+            myViewService: DefaultMyViewService(
+                databaseRepository: RealmRepository.shared,
+                localNotificationManager: DefaultLocalNotificationManager.shared,
+                calendarManager: DefaultCalendarManager()
+            )
+        )
+    )
+}
