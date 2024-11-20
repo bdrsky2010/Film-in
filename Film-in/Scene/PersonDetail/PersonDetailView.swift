07d73785ea7b95b026721163c0ec32157076036d
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-11-20 22:05:44 +0900

    🩹 Refactor: 뷰 메서드로 분리

--- Film-in/Scene/PersonDetail/PersonDetailView.swift
+++ Film-in/Scene/PersonDetail/PersonDetailView.swift
@@ -29,65 +29,7 @@ struct PersonDetailView: View {
                         .frame(width: width)
                         .padding(.top, 80)
                 } else {
-                    VStack {
-                        let url = URL(string: ImageURL.tmdb(image: viewModel.output.personDetail.profilePath).urlString)
-                        PosterImage(
-                            url: url,
-                            size: CGSize(width: width, height: width * 1.5),
-                            title: viewModel.output.personDetail.name
-                        )
-                        .grayscale(colorScheme == .dark ? 1 : 0)
-                        
-                        LazyVStack {
-                            Text(viewModel.output.personDetail.name)
-                                .font(.ibmPlexMonoSemiBold(size: 30))
-                                .foregroundStyle(.appText)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            
-                            VStack {
-                                Text(viewModel.output.personDetail.birthday)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                Text(viewModel.output.personDetail.placeOfBirth)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                            }
-                            .font(.ibmPlexMonoMedium(size: 18))
-                            .foregroundStyle(.appText)
-                            
-                            InfoHeader(titleKey: "filmography")
-                                .padding(.vertical)
-                            LazyVGrid(columns: gridItemLayout, spacing: 8) {
-                                ForEach(viewModel.output.personMovie.movies, id: \.id) { movie in
-                                    NavigationLink {
-                                        LazyView(
-                                            MovieDetailView(
-                                                movie: .init(_id: movie.id, title: movie.title, poster: movie.poster, backdrop: movie.backdrop),
-                                                size: posterSize,
-                                                viewModel: MovieDetailViewModel(
-                                                    movieDetailService: DefaultMovieDetailService(
-                                                        tmdbRepository: DefaultTMDBRepository.shared,
-                                                        databaseRepository: RealmRepository.shared
-                                                    ),
-                                                    networkMonitor: NetworkMonitor.shared,
-                                                    movieId: movie.id
-                                                )
-                                            )
-                                        )
-                                    } label: {
-                                        let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
-                                        PosterImage(
-                                            url: url,
-                                            size: CGSize(width: gridWidth, height: gridHeight),
-                                            title: movie.title
-                                        )
-                                        .padding(.bottom, 4)
-                                        .padding(.horizontal, 8)
-                                    }
-                                }
-                            }
-                        }
-                        .padding()
-                    }
-                    
+                    infoSection(width: width, gridSize: CGSize(width: gridWidth, height: gridHeight))
                 }
             }
             .task {
@@ -103,6 +45,82 @@ struct PersonDetailView: View {
         }
         .apiRequestErrorAlert(isPresented: $viewModel.output.isShowAlert) {
             viewModel.action(.refresh)
+        }
+    }
+    
+    @ViewBuilder
+    private func infoSection(width: CGFloat, gridSize: CGSize) -> some View {
+        VStack {
+            mainImageSection(width: width)
+            
+            LazyVStack {
+                keyInfoSection()
+                filmographySection(size: gridSize)
+            }
+            .padding()
+        }
+    }
+    
+    @ViewBuilder
+    private func mainImageSection(width: CGFloat) -> some View {
+        let url = URL(string: ImageURL.tmdb(image: viewModel.output.personDetail.profilePath).urlString)
+        PosterImage(
+            url: url,
+            size: CGSize(width: width, height: width * 1.5),
+            title: viewModel.output.personDetail.name
+        )
+        .grayscale(colorScheme == .dark ? 1 : 0)
+    }
+    
+    @ViewBuilder
+    private func keyInfoSection() -> some View {
+        Text(viewModel.output.personDetail.name)
+            .font(.ibmPlexMonoSemiBold(size: 30))
+            .foregroundStyle(.appText)
+            .frame(maxWidth: .infinity, alignment: .leading)
+        
+        VStack {
+            Text(viewModel.output.personDetail.birthday)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text(viewModel.output.personDetail.placeOfBirth)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .font(.ibmPlexMonoMedium(size: 18))
+        .foregroundStyle(.appText)
+    }
+    
+    @ViewBuilder
+    private func filmographySection(size: CGSize) -> some View {
+        InfoHeader(titleKey: "filmography")
+            .padding(.vertical)
+        LazyVGrid(columns: gridItemLayout, spacing: 8) {
+            ForEach(viewModel.output.personMovie.movies, id: \.id) { movie in
+                NavigationLink {
+                    LazyView(
+                        MovieDetailView(
+                            movie: .init(_id: movie.id, title: movie.title, poster: movie.poster, backdrop: movie.backdrop),
+                            size: posterSize,
+                            viewModel: MovieDetailViewModel(
+                                movieDetailService: DefaultMovieDetailService(
+                                    tmdbRepository: DefaultTMDBRepository.shared,
+                                    databaseRepository: RealmRepository.shared
+                                ),
+                                networkMonitor: NetworkMonitor.shared,
+                                movieId: movie.id
+                            )
+                        )
+                    )
+                } label: {
+                    let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
+                    PosterImage(
+                        url: url,
+                        size: size,
+                        title: movie.title
+                    )
+                    .padding(.bottom, 4)
+                    .padding(.horizontal, 8)
+                }
+            }
         }
     }
 }
