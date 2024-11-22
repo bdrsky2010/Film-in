057b53219ece1b443af686585da4e5c4d00b7aac
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-11-22 22:36:56 +0900

    💾 Add: 뷰모디파이어를 활용하여 재사용되는 커스텀 팝업 알림창을 view extension에 구현

--- Film-in/Common/Reusable/PopupAlert.swift
+++ Film-in/Common/Reusable/PopupAlert.swift
@@ -2,61 +2,142 @@
 //  PopupAlert.swift
 //  Film-in
 //
-//  Created by Minjae Kim on 11/19/24.
+//  Created by Minjae Kim on 11/22/24.
 //
 
 import SwiftUI
 import PopupView
 
-extension View {
-    func apiRequestErrorAlert(isPresented: Binding<Bool>, refreshButtonTapped: (() -> Void)? = nil) -> some View {
-        self.modifier(ApiRequestErrorAlertViewModifier(isPresented: isPresented, refreshButtonTapped: refreshButtonTapped))
+protocol PopupAlertModelType {
+    var phrase: LocalizedStringKey { get set }
+    var normal: LocalizedStringKey { get set }
+    var cancel: LocalizedStringKey? { get set }
+}
+
+struct PopupAlertModel: PopupAlertModelType {
+    var phrase: LocalizedStringKey
+    var normal: LocalizedStringKey
+    var cancel: LocalizedStringKey?
+    
+    init(
+        phrase: LocalizedStringKey,
+        normal: LocalizedStringKey,
+        cancel: LocalizedStringKey? = nil
+    ) {
+        self.phrase = phrase
+        self.normal = normal
+        self.cancel = cancel
     }
 }
 
-fileprivate struct ApiRequestErrorAlertViewModifier: ViewModifier {
+enum PopupAlertHeight {
+    case normal
+    case middle
+    case custom(_ height: CGFloat)
+}
+
+extension View {
+    func popupAlert<Model: PopupAlertModelType>(
+        isPresented: Binding<Bool>,
+        contentModel: Model,
+        height: PopupAlertHeight,
+        normalButtonTapped: (() -> Void)? = nil
+    ) -> some View {
+        self.modifier(
+            PopupAlertViewModifier(
+                isPresented: isPresented,
+                contentModel: contentModel,
+                height: height,
+                normalButtonTapped: normalButtonTapped
+            )
+        )
+    }
+}
+
+fileprivate struct PopupAlertViewModifier<Model: PopupAlertModelType>: ViewModifier {
     @Binding var isPresented: Bool
     
-    var refreshButtonTapped: (() -> Void)?
+    private let contentModel: Model
+    private let height: PopupAlertHeight
+    private var normalButtonTapped: (() -> Void)?
+    
+    init(
+        isPresented: Binding<Bool>,
+        contentModel: Model,
+        height: PopupAlertHeight,
+        normalButtonTapped: (() -> Void)? = nil
+    ) {
+        self._isPresented = isPresented
+        self.contentModel = contentModel
+        self.height = height
+        self.normalButtonTapped = normalButtonTapped
+    }
     
     func body(content: Content) -> some View {
         content
             .popup(isPresented: $isPresented) {
-                ApiRequestErrorAlert(refreshButtonTapped: refreshButtonTapped)
+                PopupAlertView(
+                    contentModel: contentModel,
+                    heightType: height,
+                    normalButtonTapped: normalButtonTapped
+                )
             } customize: {
                 $0
-                    .type(.floater())
-                    .isOpaque(true)
-                    .animation(.bouncy)
-                    .position(.top)
-                    .dragToDismiss(true)
+                    .closeOnTapOutside(true)
+                    .backgroundColor(.appText.opacity(0.5))
             }
     }
 }
 
-fileprivate struct ApiRequestErrorAlert: View {
-    var refreshButtonTapped: (() -> Void)?
+struct PopupAlertView<Model: PopupAlertModelType>: View {
+    private let contentModel: Model
+    private let height: CGFloat
+    private var normalButtonTapped: (() -> Void)?
     
-    init(refreshButtonTapped: (() -> Void)? = nil) {
-        self.refreshButtonTapped = refreshButtonTapped
+    init(
+        contentModel: Model,
+        heightType: PopupAlertHeight,
+        normalButtonTapped: ( () -> Void)? = nil
+    ) {
+        self.contentModel = contentModel
+        self.normalButtonTapped = normalButtonTapped
+        
+        switch heightType {
+        case .normal:
+            self.height = 120
+        case .middle:
+            self.height = 200
+        case .custom(let height):
+            self.height = height
+        }
     }
     
     var body: some View {
         VStack {
-            Text("apiRequestError")
+            Text(contentModel.phrase)
                 .font(.ibmPlexMonoSemiBold(size: 20))
-                .padding(.top)
-            Button {
-                refreshButtonTapped?()
-            } label: {
-                Text("refresh")
-                    .font(.ibmPlexMonoMedium(size: 20))
-                    .underline()
-                    .foregroundStyle(.white)
+            
+            Spacer()
+            
+            HStack {
+                if let cancel = contentModel.cancel {
+                    Button {
+                    } label: {
+                        Text(cancel)
+                            .appButtonText()
+                    }
+                }
+                
+                Button {
+                    normalButtonTapped?()
+                } label: {
+                    Text(contentModel.normal)
+                        .appButtonText()
+                }
             }
         }
-        .padding(.horizontal)
-        .padding(.bottom)
-        .background(.app)
+        .frame(width: 300, height: height, alignment: .top)
+        .padding()
+        .background(.background)
     }
 }
