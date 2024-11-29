4d05b16589e8eadbd192455ce80cb04742a9b1b2
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-11-29 18:32:43 +0900

    💾 Add: Custom Image Downsampling 구현
    downsampling 메서드를 구현하여 Kingfisher에 적용

--- Film-in/Common/Reusable/PosterImageView.swift
+++ Film-in/Common/Reusable/PosterImageView.swift
@@ -6,24 +6,86 @@
 //
 
 import SwiftUI
+import ImageIO
 import Kingfisher
 
 struct PosterImage: View {
     let url: URL?
     let size: CGSize
     let title: String
+    let isDownsampling: Bool
     
     var body: some View {
-        KFImage(url)
-            .resizable()
-            .placeholder{
-                Text(title)
-                    .foregroundStyle(.appText)
-            }
-            .cancelOnDisappear(true)
-            .fade(duration: 0.25)
-            .aspectRatio(contentMode: .fill)
-            .frame(width: size.width, height: size.height)
-            .clipped()
+        if isDownsampling {
+            KFImage(url)
+                .resizable()
+                .setProcessor(DownsampleProcessor(pointSize: size))
+                .placeholder{
+                    Text(title)
+                        .foregroundStyle(.appText)
+                }
+                .cacheOriginalImage()
+                .cancelOnDisappear(true)
+                .fade(duration: 0.25)
+                .aspectRatio(contentMode: .fill)
+                .frame(width: size.width, height: size.height)
+                .clipped()
+        } else {
+            KFImage(url)
+                .resizable()
+                .placeholder{
+                    Text(title)
+                        .foregroundStyle(.appText)
+                }
+                .cacheOriginalImage()
+                .cancelOnDisappear(true)
+                .fade(duration: 0.25)
+                .aspectRatio(contentMode: .fill)
+                .frame(width: size.width, height: size.height)
+                .clipped()
+        }
+    }
+}
+
+struct DownsampleProcessor: ImageProcessor {
+    let identifier: String
+    let pointSize: CGSize
+    
+    init(pointSize: CGSize) {
+        self.identifier = "com.example.DownsampleProcessor.\(pointSize)"
+        self.pointSize = pointSize
+    }
+
+    func process(item: ImageProcessItem, options: KingfisherParsedOptionsInfo) -> KFCrossPlatformImage? {
+        switch item {
+        case .image(let image):
+            guard let data = image.pngData() else { return nil }
+            return downsampling(data: data, pointSize: pointSize, scale: UITraitCollection.current.displayScale)
+        case .data(let data):
+            return downsampling(data: data, pointSize: pointSize, scale: UITraitCollection.current.displayScale)
+        }
+    }
+
+    private func downsampling(data: Data, pointSize: CGSize, scale: CGFloat) -> KFCrossPlatformImage? {
+        let imageSourceOption = [kCGImageSourceShouldCache: false] as CFDictionary
+        guard let imageSource = CGImageSourceCreateWithData(data as CFData, imageSourceOption) else {
+            print("\(#function) -> imageSource exit")
+            return nil
+        }
+        
+        let maxDimensionsInPixels = max(pointSize.width, pointSize.height) * scale
+        let downsampleOptions = [
+            kCGImageSourceCreateThumbnailFromImageAlways: true,
+            kCGImageSourceShouldCacheImmediately: true,
+            kCGImageSourceCreateThumbnailWithTransform: true,
+            kCGImageSourceThumbnailMaxPixelSize: maxDimensionsInPixels
+        ] as CFDictionary
+        
+        guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions) else {
+            print("\(#function) -> downsampledImage exit")
+            return nil
+        }
+        
+        return UIImage(cgImage: downsampledImage)
     }
 }
