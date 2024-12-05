bc157cedc17f0d9d7e469e7f1e3331712f7c926d
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-12-05 18:14:43 +0900

    💾 Add: 검색화면 서비스, 뷰모델 객체 구현 및 뷰에 적용

--- Film-in/Presentation/Search/SearchView.swift
+++ Film-in/Presentation/Search/SearchView.swift
@@ -12,6 +12,10 @@ enum FocusField {
 }
 
 struct SearchView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    @StateObject private var viewModel: SearchViewModel
+    
     @State private var cellSize: CGSize = .zero
     @State private var posterSize: CGSize = .zero
     
@@ -21,96 +25,32 @@ struct SearchView: View {
     
     @State private var isShowSearch = false
     @State private var searchQuery = ""
+    
+    init(
+        viewModel: SearchViewModel
+    ) {
+        self._viewModel = StateObject(wrappedValue: viewModel)
+    }
         
     var body: some View {
+        NavigationStack {
+            if !viewModel.output.networkConnect {
+                UnnetworkedView(refreshAction: viewModel.action(.refresh))
+            } else {
+                contentSection()
+            }
+        }
+        .task {
+            viewModel.action(.viewOnTask)
+        }
+    }
+    
+    @ViewBuilder
+    private func contentSection() -> some View {
         ZStack {
             VStack {
-                HStack {
-                    HStack {
-                        TextField("searchPlaceholder", text: $searchQuery)
-                            .focused($focusedField, equals: .cover)
-                            .padding()
-                            .font(.ibmPlexMonoSemiBold(size: 16))
-                        
-                        if !searchQuery.isEmpty {
-                            Button {
-                                searchQuery = ""
-                            } label: {
-                                Image(systemName: "xmark.app.fill")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                    .foregroundStyle(.app)
-                                    .padding(.trailing)
-                            }
-                        }
-                    }
-                    .overlay {
-                        Rectangle()
-                            .stroke(lineWidth: 3)
-                            .matchedGeometryEffect(id: "TextField", in: namespace)
-                    }
-                }
-                .padding(.horizontal)
-                
-                GeometryReader { proxy in
-                    ScrollView(.vertical) {
-                        ForEach(SearchTab.allCases, id: \.self) { tab in
-                            VStack {
-                                HStack {
-                                    Text(verbatim: tab.description)
-                                        .font(.ibmPlexMonoSemiBold(size: 20))
-                                        .bold()
-                                        .foregroundStyle(.appText)
-                                        .padding(.bottom, 4)
-                                        .matchedGeometryEffect(id: "\(tab.description)", in: namespace)
-                                }
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.horizontal, 20)
-                                
-                                ScrollView(.horizontal, showsIndicators: false) {
-                                    LazyHStack(spacing: 0) {
-                                        switch tab {
-                                        case .movie:
-                                            ForEach(0..<10) { _ in
-                                                Rectangle()
-                                                    .frame(width: cellSize.width, height: cellSize.height)
-                                                    .foregroundStyle(Color.init(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1)))
-                                                    .padding(.horizontal, 4)
-                                            }
-                                        case .person:
-                                            ForEach(0..<10) { _ in
-                                                VStack {
-                                                    Capsule()
-                                                        .frame(width: 90, height: 90)
-                                                    Text(verbatim: "이름\n이름")
-                                                        .font(.ibmPlexMonoRegular(size: 14))
-                                                        .foregroundStyle(.appText)
-                                                        .frame(width: 90)
-                                                }
-                                                .padding(.horizontal, 4)
-                                            }
-                                        }
-                                    }
-                                    .padding(.horizontal)
-                                }
-                            }
-                            .padding(.bottom)
-                        }
-                        .background(.background)
-                    }
-                    .task {
-                        if cellSize == .zero {
-                            cellSize = CGSize(
-                                width: (proxy.size.width - (8 * 2)) / 3 * 0.8,
-                                height: (proxy.size.width - (8 * 2)) / 3 * 1.5 * 0.8
-                            )
-                        }
-                        
-                        if posterSize == .zero {
-                            posterSize = CGSize(width: proxy.size.width * 0.7, height: proxy.size.width * 0.7 * 1.5)
-                        }
-                    }
-                }
+                textFieldSection()
+                trendingContentSection()
             }
             
             if isShowSearch {
@@ -130,9 +70,133 @@ struct SearchView: View {
                 }
             }
         }
+        .apiRequestErrorAlert(isPresented: $viewModel.output.isShowAlert) {
+            viewModel.action(.refresh)
+        }
+    }
+    
+    @ViewBuilder
+    private func textFieldSection() -> some View {
+        HStack {
+            TextField("searchPlaceholder", text: $searchQuery)
+                .focused($focusedField, equals: .cover)
+                .padding()
+                .font(.ibmPlexMonoSemiBold(size: 16))
+            
+            if !searchQuery.isEmpty {
+                Button {
+                    searchQuery = ""
+                } label: {
+                    Image(systemName: "xmark.app.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundStyle(.app)
+                        .padding(.trailing)
+                }
+            }
+        }
+        .overlay {
+            Rectangle()
+                .stroke(lineWidth: 3)
+                .matchedGeometryEffect(id: "TextField", in: namespace)
+        }
+        .padding(.horizontal)
+    }
+    
+    @ViewBuilder
+    private func trendingContentSection() -> some View {
+        GeometryReader { proxy in
+            ScrollView(.vertical) {
+                ForEach(SearchTab.allCases, id: \.self) { tab in
+                    VStack {
+                        HStack {
+                            Text(verbatim: tab.description)
+                                .font(.ibmPlexMonoSemiBold(size: 20))
+                                .bold()
+                                .foregroundStyle(.appText)
+                                .padding(.bottom, 4)
+                                .matchedGeometryEffect(id: "\(tab.description)", in: namespace)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal, 20)
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            LazyHStack(spacing: 8) {
+                                switch tab {
+                                case .movie:
+                                    trendingMovieSection()
+                                case .person:
+                                    trendingPeopleSection()
+                                }
+                            }
+                            .padding(.horizontal)
+                        }
+                    }
+                    .padding(.bottom)
+                }
+                .background(.background)
+            }
+            .refreshable {
+                viewModel.action(.refresh)
+            }
+            .task {
+                if cellSize == .zero {
+                    cellSize = CGSize(
+                        width: (proxy.size.width - (8 * 2)) / 3,
+                        height: (proxy.size.width - (8 * 2)) / 3 * 1.5
+                    )
+                }
+                
+                if posterSize == .zero {
+                    posterSize = CGSize(width: proxy.size.width * 0.7, height: proxy.size.width * 0.7 * 1.5)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func trendingMovieSection() -> some View {
+        ForEach(viewModel.output.trendingMovie, id: \.id) { movie in
+            let url = URL(string: ImageURL.tmdb(image: movie.poster).urlString)
+            PosterImage(
+                url: url,
+                size: cellSize,
+                title: movie.title,
+                isDownsampling: true
+            )
+        }
+    }
+    
+    @ViewBuilder
+    private func trendingPeopleSection() -> some View {
+        ForEach(viewModel.output.trendingPeople, id: \.id) { person in
+            VStack {
+                let url = URL(string: ImageURL.tmdb(image: person.profilePath).urlString)
+                PosterImage(
+                    url: url,
+                    size: CGSize(width: 90, height: 90),
+                    title: person.name.replacingOccurrences(of: " ", with: "\n"),
+                    isDownsampling: true
+                )
+                .clipShape(Circle())
+                .grayscale(colorScheme == .dark ? 1 : 0)
+                
+                Text("\(person.name)")
+                    .font(.ibmPlexMonoRegular(size: 14))
+                    .foregroundStyle(.appText)
+                    .frame(width: 90)
+            }
+            .padding(.horizontal, 4)
+        }
     }
 }
 
 #Preview {
-    SearchView()
+    SearchView(
+        viewModel: SearchViewModel(
+            searchSerivce: DefaultSearchService(
+                tmdbRepository: DefaultTMDBRepository.shared
+            ),
+            networkMonitor: NetworkMonitor.shared)
+    )
 }
---
+++ Film-in/Presentation/Search/SearchViewModel.swift
@@ -0,0 +1,143 @@
+//
+//  SearchViewModel.swift
+//  Film-in
+//
+//  Created by Minjae Kim on 12/1/24.
+//
+
+import Foundation
+import Combine
+
+final class SearchViewModel: BaseObject, ViewModelType {
+    private let searchSerivce: SearchService
+    private let networkMonitor: NetworkMonitor
+    
+    private var loadData: [String : Bool] = [:]
+    
+    @Published var output = Output()
+    
+    var input = Input()
+    var cancellable = Set<AnyCancellable>()
+    
+    init(
+        searchSerivce: SearchService,
+        networkMonitor: NetworkMonitor
+    ) {
+        self.searchSerivce = searchSerivce
+        self.networkMonitor = networkMonitor
+        super.init()
+        transform()
+    }
+}
+
+extension SearchViewModel {
+    struct Input {
+        let viewOnTask = PassthroughSubject<Void, Never>()
+        let refresh = PassthroughSubject<Void, Never>()
+    }
+    
+    struct Output {
+        var networkConnect = true
+        var isShowAlert = false
+        var trendingMovie = [MovieData]()
+        var trendingPeople = [TrendingPerson]()
+    }
+    
+    func transform() {
+        input.viewOnTask
+            .sink { [weak self] in
+                guard let self else { return }
+                fetchData()
+            }
+            .store(in: &cancellable)
+        
+        input.refresh
+            .sink { [weak self] in
+                guard let self else { return }
+                loadData = [:]
+                fetchData()
+            }
+            .store(in: &cancellable)
+    }
+}
+
+extension SearchViewModel {
+    enum Action {
+        case viewOnTask
+        case refresh
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .viewOnTask:
+            input.viewOnTask.send(())
+        case .refresh:
+            input.refresh.send(())
+        }
+    }
+}
+
+extension SearchViewModel {
+    private func fetchData() {
+        guard networkMonitor.networkType != .notConnect else {
+            output.networkConnect = false
+            return
+        }
+        output.networkConnect = true
+        
+        guard loadData.count == 0 else { return }
+        
+        fetchTrendingMovie()
+        fetchTrendingPeople()
+    }
+    
+    private func fetchTrendingMovie() {
+        let query = TrendingQuery(language: "longLanguageCode".localized)
+        let publisher = searchSerivce.fetchTrendingMovie(query: query)
+        publisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] result in
+                guard let self else { return }
+                switch result {
+                case .success(let movie):
+                    output.trendingMovie = movie
+                    dataLoad(for: #function)
+                case .failure(let error):
+                    print(error)
+                    errorHandling()
+                }
+            }
+            .store(in: &cancellable)
+    }
+    
+    private func fetchTrendingPeople() {
+        let query = TrendingQuery(language: "longLanguageCode".localized)
+        let publisher = searchSerivce.fetchTrendingPeople(query: query)
+        publisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] result in
+                guard let self else { return }
+                switch result {
+                case .success(let people):
+                    output.trendingPeople = people
+                    dataLoad(for: #function)
+                case .failure(let error):
+                    print(error)
+                    errorHandling()
+                }
+            }
+            .store(in: &cancellable)
+    }
+}
+
+extension SearchViewModel {
+    private func errorHandling() {
+        if !output.isShowAlert {
+            output.isShowAlert = true
+        }
+    }
+    
+    private func dataLoad(for key: String) {
+        loadData[key] = true
+    }
+}
