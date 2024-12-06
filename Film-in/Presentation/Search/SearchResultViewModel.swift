e5e3105244da02d289fecc8e39868acfe168b211
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-12-06 23:01:08 +0900

    💾 Add: 검색결과화면 뷰모델 구성중
     - 검색어 변경 액션 / 검색어 검색 액션 Combine 활용해 비동기 바인딩

---
+++ Film-in/Presentation/Search/SearchResultViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  SearchResultViewModel.swift
+//  Film-in
+//
+//  Created by Minjae Kim on 12/5/24.
+//
+
+import Foundation
+import Combine
+
+final class SearchResultViewModel: BaseObject, ViewModelType {
+    private let networkMonitor: NetworkMonitor
+    
+    @Published var output = Output()
+
+    var input = Input()
+    var cancellable = Set<AnyCancellable>()
+    
+    init(
+        networkMonitor: NetworkMonitor
+    ) {
+        self.networkMonitor = networkMonitor
+        super.init()
+        transform()
+    }
+}
+
+extension SearchResultViewModel {
+    struct Input {
+        var onChangeSearchQuery = PassthroughSubject<String, Never>()
+        var onSubmitSearchQuery = PassthroughSubject<String, Never>()
+    }
+    
+    struct Output {
+        var searchQuery = ""
+    }
+    
+    func transform() {
+        input.onChangeSearchQuery
+            .debounce(for: .seconds(0.5), scheduler: RunLoop.main)
+            .sink { query in
+                print(query)
+            }
+            .store(in: &cancellable)
+        
+        input.onSubmitSearchQuery
+            .throttle(for: 10, scheduler: RunLoop.main, latest: true)
+            .sink { query in
+                print(query)
+            }
+            .store(in: &cancellable)
+    }
+}
+
+extension SearchResultViewModel {
+    enum Action {
+        case onChangeSearchQuery(_ query: String)
+        case onSubmitSearchQuery(_ query: String)
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .onChangeSearchQuery(let query):
+            input.onChangeSearchQuery.send(query)
+        case .onSubmitSearchQuery(let query):
+            input.onSubmitSearchQuery.send(query)
+        }
+    }
+}
