61805559b78fd502f7a1f143daa93294376290de
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-12-08 23:51:34 +0900

    💾 Add: 뷰모델 프로퍼티, Input, Output, Scription, Action, 메서드 추가 구현
    - 프로퍼티
      - previousQuery: 이전 검색어와 비교해서 동일한 검색어일 경우 API 요청을 하지 않기 위함
    
    - Input
      - onFocusTextField: 서치바에 포커싱이 된 경우
      - onDismiss: 뷰가 사라져야 하는 경우
      - getRandomSearchQuery: 빈 검색어에 랜덤 검색어를 넣어주기 위함
    
    - Output
      - isSearched: 검색 버튼을 눌러서 검색을 요청했는 지 유무
      - isFetching: 검색중 즉, API 요청해서 데이터 받아오고 있는 지 유무
      - randomSearchQuery: 빈 검색어에 랜덤 검색어를 넣어주기 위함
    
    - Action(Input과 동일)
      - onFocusTextField
      - onDismiss
      - getRandomSearchQuery
    
    - 메서드
      - getRandomMCUMovie: 빈 검색어에 랜덤 검색어를 넣어주기 위함

--- Film-in/Presentation/Search/SearchResultViewModel.swift
+++ Film-in/Presentation/Search/SearchResultViewModel.swift
@@ -11,6 +11,8 @@ import Combine
 final class SearchResultViewModel: BaseObject, ViewModelType {
     private let networkMonitor: NetworkMonitor
     
+    private var previousQuery = ""
+    
     @Published var output = Output()
 
     var input = Input()
@@ -27,14 +29,27 @@ final class SearchResultViewModel: BaseObject, ViewModelType {
 
 extension SearchResultViewModel {
     struct Input {
+        var onFocusTextField = PassthroughSubject<Void, Never>()
         var onChangeSearchQuery = PassthroughSubject<String, Never>()
         var onSubmitSearchQuery = PassthroughSubject<String, Never>()
+        var onDismiss = PassthroughSubject<Void, Never>()
+        var getRandomSearchQuery = PassthroughSubject<Void, Never>()
     }
     
     struct Output {
+        var isSearched = false
+        var isFetching = false
+        
+        var randomSearchQuery = ""
     }
     
     func transform() {
+        input.onFocusTextField
+            .sink(with: self) { owner, _ in
+                owner.output.isSearched = false
+            }
+            .store(in: &cancellable)
+        
         input.onChangeSearchQuery
             .debounce(for: .seconds(0.5), scheduler: RunLoop.main)
             .sink { query in
@@ -46,6 +61,30 @@ extension SearchResultViewModel {
             .throttle(for: 10, scheduler: RunLoop.main, latest: true)
             .sink { query in
                 print(query)
+            .sink(with: self) { owner, query in
+                owner.output.isSearched = true
+                
+                if owner.previousQuery != query {
+                    owner.output.isFetching = true
+                    Task {
+                        // TODO: TMDB Search(Movie / People) API Request
+                        try await Task.sleep(nanoseconds: 1_500_000_000)
+                        owner.output.isFetching = false
+                    }
+                    owner.previousQuery = query
+                }
+            }
+            .store(in: &cancellable)
+        
+        input.onDismiss
+            .sink(with: self) { owner, _ in
+                owner.output.isSearched = true
+            }
+            .store(in: &cancellable)
+        
+        input.getRandomSearchQuery
+            .sink(with: self) { owner, _ in
+                owner.output.randomSearchQuery = owner.getRandomMCUMovie()
             }
             .store(in: &cancellable)
     }
@@ -53,16 +92,67 @@ extension SearchResultViewModel {
 
 extension SearchResultViewModel {
     enum Action {
+        case onFocusTextField
         case onChangeSearchQuery(_ query: String)
         case onSubmitSearchQuery(_ query: String)
+        case onDismiss
+        case getRandomSearchQuery
     }
     
     func action(_ action: Action) {
         switch action {
+        case .onFocusTextField:
+            input.onFocusTextField.send(())
         case .onChangeSearchQuery(let query):
             input.onChangeSearchQuery.send(query)
         case .onSubmitSearchQuery(let query):
             input.onSubmitSearchQuery.send(query)
+        case .onDismiss:
+            input.onDismiss.send(())
+        case .getRandomSearchQuery:
+            input.getRandomSearchQuery.send(())
         }
+    }
+}
+
+extension SearchResultViewModel {
+    private func getRandomMCUMovie() -> String {
+        let mcuMovies: [String] = [
+            "Iron Man",
+            "The Incredible Hulk",
+            "Iron Man 2",
+            "Thor",
+            "Captain America: The First Avenger",
+            "The Avengers",
+            "Iron Man 3",
+            "Thor: The Dark World",
+            "Captain America: The Winter Soldier",
+            "Guardians of the Galaxy",
+            "Avengers: Age of Ultron",
+            "Ant-Man",
+            "Captain America: Civil War",
+            "Doctor Strange",
+            "Guardians of the Galaxy Vol. 2",
+            "Spider-Man: Homecoming",
+            "Thor: Ragnarok",
+            "Black Panther",
+            "Avengers: Infinity War",
+            "Ant-Man and the Wasp",
+            "Captain Marvel",
+            "Avengers: Endgame",
+            "Spider-Man: Far From Home",
+            "Black Widow",
+            "Shang-Chi and the Legend of the Ten Rings",
+            "Eternals",
+            "Spider-Man: No Way Home",
+            "Doctor Strange in the Multiverse of Madness",
+            "Thor: Love and Thunder",
+            "Black Panther: Wakanda Forever",
+            "Ant-Man and the Wasp: Quantumania",
+            "Guardians of the Galaxy Vol. 3",
+            "The Marvels"
+        ]
+        
+        return mcuMovies.randomElement() ?? "Spider Man"
     }
 }
