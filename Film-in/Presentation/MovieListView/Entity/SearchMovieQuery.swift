90c28c42ae478681ce1b0a1ba5d9eb66e7f08b6d
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-12-10 22:02:27 +0900

    💾 Add: 영화, 인물 검색 쿼리 타입 정의

---
+++ Film-in/Presentation/MovieListView/Entity/SearchMovieQuery.swift
@@ -0,0 +1,15 @@
+//
+//  SearchMovieQuery.swift
+//  Film-in
+//
+//  Created by Minjae Kim on 12/10/24.
+//
+
+import Foundation
+
+struct SearchMovieQuery {
+    let query: String
+    let language: String
+    let page: Int
+    let region: String
+}
