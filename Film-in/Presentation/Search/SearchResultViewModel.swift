3d45cce261f4387cf81d6c100eb76b640e63e005
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-12-10 12:26:40 +0900

    🩹 Refactor: 검색어 없이 입력 시, 랜덤 검색어 보내주는 코드 서비스 객체로 이동

--- Film-in/Presentation/Search/SearchResultViewModel.swift
+++ Film-in/Presentation/Search/SearchResultViewModel.swift
@@ -107,7 +107,7 @@ extension SearchResultViewModel {
         
         input.getRandomSearchQuery
             .sink(with: self) { owner, _ in
-                owner.output.randomSearchQuery = owner.getRandomMCUMovie()
+                owner.output.randomSearchQuery = owner.searchResultService.getRandomMCUMovie()
             }
             .store(in: &cancellable)
     }
@@ -142,44 +142,6 @@ extension SearchResultViewModel {
 }
 
 extension SearchResultViewModel {
-    private func getRandomMCUMovie() -> String {
-        let mcuMovies: [String] = [
-            "Iron Man",
-            "The Incredible Hulk",
-            "Iron Man 2",
-            "Thor",
-            "Captain America: The First Avenger",
-            "The Avengers",
-            "Iron Man 3",
-            "Thor: The Dark World",
-            "Captain America: The Winter Soldier",
-            "Guardians of the Galaxy",
-            "Avengers: Age of Ultron",
-            "Ant-Man",
-            "Captain America: Civil War",
-            "Doctor Strange",
-            "Guardians of the Galaxy Vol. 2",
-            "Spider-Man: Homecoming",
-            "Thor: Ragnarok",
-            "Black Panther",
-            "Avengers: Infinity War",
-            "Ant-Man and the Wasp",
-            "Captain Marvel",
-            "Avengers: Endgame",
-            "Spider-Man: Far From Home",
-            "Black Widow",
-            "Shang-Chi and the Legend of the Ten Rings",
-            "Eternals",
-            "Spider-Man: No Way Home",
-            "Doctor Strange in the Multiverse of Madness",
-            "Thor: Love and Thunder",
-            "Black Panther: Wakanda Forever",
-            "Ant-Man and the Wasp: Quantumania",
-            "Guardians of the Galaxy Vol. 3",
-            "The Marvels"
-        ]
-        
-        return mcuMovies.randomElement() ?? "Spider Man"
     private func networkHandling() {
         guard networkMonitor.networkType != .notConnect else {
             output.networkConnect = false
