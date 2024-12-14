9776c6f9a6519487c164354b1833cc7b5f46c4c7
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-12-14 20:06:03 +0900

    🩹 Refactor: 메서드로 뷰 분리

--- Film-in/Presentation/Search/SearchResultView.swift
+++ Film-in/Presentation/Search/SearchResultView.swift
@@ -50,222 +50,244 @@ struct SearchResultView: View {
     }
     
     var body: some View {
+        contentSection()
+            .background(.background)
+            .valueChanged(value: focusedField.wrappedValue) { _ in
+                if focusedField.wrappedValue == .search {
+                    viewModel.action(.onFocusTextField)
+                }
+            }
+            .valueChanged(value: searchQuery) { query in
+                viewModel.action(.onChangeSearchQuery(query))
+            }
+            .popupAlert(
+                isPresented: Binding(
+                    get: { viewModel.output.isShowAlert },
+                    set: { _ in viewModel.action(.onDismissAlert) }
+                ),
+                contentModel: .init(
+                    systemImage: "wifi.exclamationmark",
+                    phrase: "apiRequestError",
+                    normal: "refresh"
+                ),
+                heightType: .middle
+            ) {
+                viewModel.action(.onChangeSearchQuery(searchQuery))
+            }
+            .popupAlert(
+                isPresented: $isShowAlert,
+                contentModel: .init(
+                    systemImage: "wifi.exclamationmark",
+                    phrase: "apiRequestError",
+                    normal: "refresh"
+                ),
+                heightType: .middle
+            ) {
+                isRefresh = true
+            }
+    }
+}
+
+extension SearchResultView {
+    @ViewBuilder
+    private func contentSection() -> some View {
         VStack {
+            searchBarSection()
+            
+            if viewModel.output.isSearched {
+                searchResultSection()
+            } else {
+                keywordListSection()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func searchBarSection() -> some View {
+        HStack {
+            if viewModel.output.isSearched {
+                Button {
+                    withAnimation {
+                        searchQuery = ""
+                        isShowSearch = false
+                    }
+                } label: {
+                    Image(systemName: "chevron.backward")
+                        .tint(.app)
+                        .font(.title.bold())
+                        .padding(.trailing, 8)
+                }
+            }
+            
             HStack {
-                if viewModel.output.isSearched {
+                TextField("searchPlaceholder", text: $searchQuery)
+                    .focused(focusedField, equals: .search)
+                    .autocorrectionDisabled()
+                    .submitLabel(.search)
+                    .padding()
+                    .font(.ibmPlexMonoSemiBold(size: 16))
+                    .onSubmit {
+                        if isFirstSearch { isFirstSearch = false }
+                        if searchQuery.isEmpty {
+                            viewModel.action(.getRandomSearchQuery)
+                            searchQuery = viewModel.output.randomSearchQuery
+                        }
+                        addRecentQuery()
+                        viewModel.action(.onSubmitSearchQuery(searchQuery))
+                    }
+                
+                if !searchQuery.isEmpty {
                     Button {
-                        withAnimation {
+                        searchQuery = ""
+                    } label: {
+                        Image(systemName: "xmark.app.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundStyle(.app)
+                            .padding(.trailing)
+                    }
+                }
+            }
+            .overlay {
+                Rectangle()
+                    .stroke(lineWidth: 3)
+                    .matchedGeometryEffect(id: "TextField", in: namespace)
+            }
+            
+            if !viewModel.output.isSearched {
+                Button {
+                    withAnimation {
+                        if isFirstSearch {
                             searchQuery = ""
                             isShowSearch = false
-                        }
-                    } label: {
-                        Image(systemName: "chevron.backward")
-                            .tint(.app)
-                            .font(.title.bold())
-                            .padding(.trailing, 8)
-                    }
-                }
-                
-                HStack {
-                    TextField("searchPlaceholder", text: $searchQuery)
-                        .focused(focusedField, equals: .search)
-                        .autocorrectionDisabled()
-                        .submitLabel(.search)
-                        .padding()
-                        .font(.ibmPlexMonoSemiBold(size: 16))
-                        .onSubmit {
-                            if isFirstSearch { isFirstSearch = false }
-                            if searchQuery.isEmpty {
-                                viewModel.action(.getRandomSearchQuery)
-                                searchQuery = viewModel.output.randomSearchQuery
-                            }
-                            recentQuery[searchQuery] = Date()
-                            viewModel.action(.onSubmitSearchQuery(searchQuery))
-                        }
-                    
-                    if !searchQuery.isEmpty {
-                        Button {
-                            searchQuery = ""
-                        } label: {
-                            Image(systemName: "xmark.app.fill")
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                                .foregroundStyle(.app)
-                                .padding(.trailing)
+                        } else {
+                            focusedField.wrappedValue = nil
+                            viewModel.action(.onDismiss)
                         }
                     }
-                }
-                .overlay {
-                    Rectangle()
-                        .stroke(lineWidth: 3)
-                        .matchedGeometryEffect(id: "TextField", in: namespace)
-                }
-                
-                if !viewModel.output.isSearched {
-                    Button {
-                        withAnimation {
-                            if isFirstSearch {
-                                searchQuery = ""
-                                isShowSearch = false
-                            } else {
-                                focusedField.wrappedValue = nil
-                                viewModel.action(.onDismiss)
-                            }
-                        }
-                    } label: {
-                        Text("cancel")
-                            .tint(.app)
-                            .font(.ibmPlexMonoSemiBold(size: 20))
-                            .padding(.leading, 8)
-                    }
+                } label: {
+                    Text("cancel")
+                        .tint(.app)
+                        .font(.ibmPlexMonoSemiBold(size: 20))
+                        .padding(.leading, 8)
                 }
             }
-            .padding(.horizontal)
-            .animation(.easeInOut, value: viewModel.output.isSearched)
-
-            if viewModel.output.isSearched {
-                VStack {
-                    HStack(spacing: 12) {
-                        ForEach(SearchType.allCases, id: \.self) { tab in
-                            Text(verbatim: tab.description)
-                                .font(.ibmPlexMonoSemiBold(size: 20))
+        }
+        .padding(.horizontal)
+        .animation(.easeInOut, value: viewModel.output.isSearched)
+    }
+    
+    @ViewBuilder
+    private func keywordListSection() -> some View {
+        List {
+            if searchQuery.isEmpty {
+                Section {
+                    ForEach(recentQuery.sorted(by: { $0.value > $1.value }), id: \.key) { query in
+                        Text(verbatim: query.key)
+                            .onTapGesture {
+                                if isFirstSearch { isFirstSearch = false }
+                                searchQuery = query.key
+                                focusedField.wrappedValue = nil
+                                recentQuery[searchQuery] = Date()
+                                viewModel.action(.onSubmitSearchQuery(searchQuery))
+                            }
+                    }
+                    .onDelete(perform: deleteRecentQuery)
+                } header: {
+                    HStack(alignment: .lastTextBaseline) {
+                        Text(verbatim: "최근 검색어")
+                            .font(.ibmPlexMonoSemiBold(size: 20))
+                            .bold()
+                            .foregroundStyle(.appText)
+                            .padding(.bottom, 4)
+                        
+                        Spacer()
+                        
+                        Button {
+                            recentQuery.removeAll()
+                        } label: {
+                            Text(verbatim: "전체삭제")
+                                .font(.ibmPlexMonoSemiBold(size: 14))
                                 .bold()
-                                .foregroundStyle(.appText)
+                                .foregroundStyle(.app)
+                                .underline()
                                 .padding(.bottom, 4)
-                                .matchedGeometryEffect(id: "\(tab.description)", in: namespace)
-                                .overlay(alignment: .bottom) {
-                                    if selection == tab {
-                                        Capsule()
-                                            .frame(height: 4)
-                                            .foregroundStyle(.app)
-                                            .matchedGeometryEffect(id: "tab", in: namespace)
-                                    }
-                                }
-                                .animation(.easeInOut, value: selection)
-                                .onTapGesture {
-                                    selection = tab
-                                }
                         }
                     }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal, 20)
-                    
-                    TabView(selection: $selection) {
-                        ForEach(SearchType.allCases, id: \.self) { tab in
-                            LazyView(tab.makeView(
-                                query: searchQuery,
-                                isShowAlert: $isShowAlert,
-                                isRefresh: $isRefresh
-                            ))
-                                .tabItem { }
-                                .tag(tab)
-                        }
-                    }
-                    .tabViewStyle(.page(indexDisplayMode: .never))
-                    .animation(.easeInOut, value: selection)
-                    .ignoresSafeArea(edges: .bottom)
                 }
             } else {
-                List {
-                    if searchQuery.isEmpty {
-                        Section {
-                            ForEach(recentQuery.sorted(by: { $0.value > $1.value }), id: \.key) { query in
-                                Text(verbatim: query.key)
-                                    .onTapGesture {
-                                        if isFirstSearch { isFirstSearch = false }
-                                        searchQuery = query.key
-                                        focusedField.wrappedValue = nil
-                                        recentQuery[searchQuery] = Date()
-                                        viewModel.action(.onSubmitSearchQuery(searchQuery))
-                                    }
-                            }
-                            .onDelete(perform: deleteRecentQuery)
-                        } header: {
-                            HStack(alignment: .lastTextBaseline) {
-                                Text(verbatim: "최근 검색어")
-                                    .font(.ibmPlexMonoSemiBold(size: 20))
-                                    .bold()
-                                    .foregroundStyle(.appText)
-                                    .padding(.bottom, 4)
-                                
-                                Spacer()
-                                
-                                Button {
-                                    recentQuery.removeAll()
-                                } label: {
-                                    Text(verbatim: "전체삭제")
-                                        .font(.ibmPlexMonoSemiBold(size: 14))
-                                        .bold()
-                                        .foregroundStyle(.app)
-                                        .underline()
-                                        .padding(.bottom, 4)
+                Section {
+                    ForEach(viewModel.output.multiSearchList, id: \.self) { item in
+                        HStack{
+                            Image(systemName: item.type == .movie ? "movieclapper.fill" : "person.fill")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                            Text(verbatim: item.keyword)
+                                .onTapGesture {
+                                    if isFirstSearch { isFirstSearch = false }
+                                    searchQuery = item.keyword
+                                    focusedField.wrappedValue = nil
+                                    recentQuery[searchQuery] = Date()
+                                    viewModel.action(.onSubmitSearchQuery(searchQuery))
                                 }
-                            }
-                        }
-                    } else {
-                        Section {
-                            ForEach(viewModel.output.multiSearchList, id: \.self) { item in
-                                HStack{
-                                    Image(systemName: item.type == .movie ? "movieclapper.fill" : "person.fill")
-                                        .resizable()
-                                        .frame(width: 20, height: 20)
-                                    Text(verbatim: item.keyword)
-                                        .onTapGesture {
-                                            if isFirstSearch { isFirstSearch = false }
-                                            searchQuery = item.keyword
-                                            focusedField.wrappedValue = nil
-                                            recentQuery[searchQuery] = Date()
-                                            viewModel.action(.onSubmitSearchQuery(searchQuery))
-                                        }
-                                }
-                            }
-                        } header: {
-                            Text(verbatim: "연관 검색어")
-                                .font(.ibmPlexMonoSemiBold(size: 20))
-                                .bold()
-                                .foregroundStyle(.appText)
-                                .padding(.bottom, 4)
                         }
                     }
+                } header: {
+                    Text(verbatim: "연관 검색어")
+                        .font(.ibmPlexMonoSemiBold(size: 20))
+                        .bold()
+                        .foregroundStyle(.appText)
+                        .padding(.bottom, 4)
                 }
-                .listStyle(.plain)
-                .scrollDismissesKeyboard(.interactively)
-                .animation(.easeInOut, value: recentQuery)
             }
         }
-        .background(.background)
-        .valueChanged(value: focusedField.wrappedValue) { _ in
-            if focusedField.wrappedValue == .search {
-                viewModel.action(.onFocusTextField)
+        .listStyle(.plain)
+        .scrollDismissesKeyboard(.interactively)
+        .animation(.easeInOut, value: recentQuery)
+    }
+    
+    @ViewBuilder
+    private func searchResultSection() -> some View {
+        VStack {
+            HStack(spacing: 12) {
+                ForEach(SearchType.allCases, id: \.self) { tab in
+                    Text(verbatim: tab.description)
+                        .font(.ibmPlexMonoSemiBold(size: 20))
+                        .bold()
+                        .foregroundStyle(.appText)
+                        .padding(.bottom, 4)
+                        .matchedGeometryEffect(id: "\(tab.description)", in: namespace)
+                        .overlay(alignment: .bottom) {
+                            if selection == tab {
+                                Capsule()
+                                    .frame(height: 4)
+                                    .foregroundStyle(.app)
+                                    .matchedGeometryEffect(id: "tab", in: namespace)
+                            }
+                        }
+                        .animation(.easeInOut, value: selection)
+                        .onTapGesture {
+                            selection = tab
+                        }
+                }
             }
-        }
-        .valueChanged(value: searchQuery) { query in
-            viewModel.action(.onChangeSearchQuery(query))
-        }
-        .popupAlert(
-            isPresented: Binding(
-                get: { viewModel.output.isShowAlert },
-                set: { _ in viewModel.action(.onDismissAlert) }
-            ),
-            contentModel: .init(
-                systemImage: "wifi.exclamationmark",
-                phrase: "apiRequestError",
-                normal: "refresh"
-            ),
-            heightType: .middle
-        ) {
-            viewModel.action(.onChangeSearchQuery(searchQuery))
-        }
-        .popupAlert(
-            isPresented: $isShowAlert,
-            contentModel: .init(
-                systemImage: "wifi.exclamationmark",
-                phrase: "apiRequestError",
-                normal: "refresh"
-            ),
-            heightType: .middle
-        ) {
-            isRefresh = true
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 20)
+            
+            TabView(selection: $selection) {
+                ForEach(SearchType.allCases, id: \.self) { tab in
+                    LazyView(tab.makeView(
+                        query: searchQuery,
+                        isShowAlert: $isShowAlert,
+                        isRefresh: $isRefresh
+                    ))
+                        .tabItem { }
+                        .tag(tab)
+                }
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .animation(.easeInOut, value: selection)
+            .ignoresSafeArea(edges: .bottom)
         }
     }
 }
