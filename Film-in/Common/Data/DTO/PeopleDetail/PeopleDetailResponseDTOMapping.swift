4819e36b197b3d58a39ce6fde15800d32763c469
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-12-17 20:39:09 +0900

    👾 Fix: 받아온 데이터가 Null 값이라서 디코딩을 못해 옵셔널 형태로 받아오도록 수정

--- Film-in/Common/Data/DTO/PeopleDetail/PeopleDetailResponseDTOMapping.swift
+++ Film-in/Common/Data/DTO/PeopleDetail/PeopleDetailResponseDTOMapping.swift
@@ -11,10 +11,10 @@ struct PeopleDetailResponseDTO: Decodable {
     let adult: Bool
     let alsoKnownAs: [String]
     let biography: String
-    let birthday: String?
-    let deathday: String?
+    let birthday: String
+    let deathday: String
     let gender: Int
-    let homepage: String?
+    let homepage: String
     let id: Int
     let imdbId: String
     let department: String
@@ -39,6 +39,24 @@ struct PeopleDetailResponseDTO: Decodable {
         case popularity
         case profilePath = "profile_path"
     }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.adult = try container.decode(Bool.self, forKey: .adult)
+        self.alsoKnownAs = try container.decode([String].self, forKey: .alsoKnownAs)
+        self.biography = try container.decode(String.self, forKey: .biography)
+        self.birthday = try container.decodeIfPresent(String.self, forKey: .birthday) ?? ""
+        self.deathday = try container.decodeIfPresent(String.self, forKey: .deathday) ?? ""
+        self.gender = try container.decode(Int.self, forKey: .gender)
+        self.homepage = try container.decodeIfPresent(String.self, forKey: .homepage) ?? ""
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.imdbId = try container.decode(String.self, forKey: .imdbId)
+        self.department = try container.decode(String.self, forKey: .department)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.placeOfBirth = try container.decodeIfPresent(String.self, forKey: .placeOfBirth) ?? ""
+        self.popularity = try container.decode(Double.self, forKey: .popularity)
+        self.profilePath = try container.decode(String.self, forKey: .profilePath)
+    }
 }
 
 extension PeopleDetailResponseDTO {
@@ -46,7 +64,7 @@ extension PeopleDetailResponseDTO {
         return PersonDetail(
             id: self.id,
             name: self.name,
-            birthday: self.birthday ?? "",
+            birthday: self.birthday,
             placeOfBirth: self.placeOfBirth,
             profilePath: self.profilePath
         )
